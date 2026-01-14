0f9c93e5601e02b10f3e612fd5fd3b6e83efb837
HIM049 <HIM_049@163.com>
2026-01-14 16:39:00 +0800

    feat: new timeline

--- src/ui/timeline.rs
+++ src/ui/timeline.rs
@@ -1,9 +1,9 @@
 use std::sync::Arc;
 
 use gpui::{
-    AbsoluteLength, App, BorderStyle, Bounds, Corners, DefiniteLength, Element, ElementId,
+    AbsoluteLength, App, BorderStyle, Bounds, Corners, DefiniteLength, Element, ElementId, Hsla,
     IntoElement, LayoutId, Length, MouseButton, MouseDownEvent, Path, Pixels, Point, Size, Style,
-    point, px, quad, relative, rgb, rgba,
+    Window, point, px, quad, relative, rgb, size,
 };
 
 pub struct Timeline {
@@ -91,44 +91,108 @@ impl Element for Timeline {
     ) {
         let scale = window.scale_factor();
 
+        let base_played = rgb(0x0091FF);
+        let base_unplay = rgb(0x005CA3);
+        let base_h = px(10.);
+
+        let selected_played = rgb(0xf0e59a);
+        let selected_unplay = rgb(0x978F5C);
+
+        let point_color = rgb(0xFFF29A);
+
+        let indi_x = self.indicator_x(bounds);
+        let indi_y = self.origin_point.y - px(14.);
+
         // timeline base
-        let timeline_h = px(10.);
         window.paint_quad(quad(
             Bounds {
                 origin: self.origin_point,
                 size: Size {
                     width: bounds.size.width,
-                    height: timeline_h,
+                    height: base_h,
                 },
             },
             Corners::default(),
-            rgb(0x65acd7),
+            base_unplay,
+            px(0.),
+            gpui::white(),
+            BorderStyle::default(),
+        ));
+        // timebase played
+        window.paint_quad(quad(
+            Bounds {
+                origin: self.origin_point,
+                size: Size {
+                    width: indi_x - px(1.),
+                    height: base_h,
+                },
+            },
+            Corners::default(),
+            base_played,
             px(0.),
             gpui::white(),
             BorderStyle::default(),
         ));
 
         // selected range
-        if let Some(start) = self.range_start {
+        if let (Some(start), Some(end)) = (self.range_start, self.range_end) {
             let point_a = (bounds.size.width * start).round();
-            if let Some(end) = self.range_end {
-                let point_b = (bounds.size.width * end).round();
+            let point_b = (bounds.size.width * end).round();
 
+            let divide_point = if indi_x > point_b {
+                // indicator in range
+                point_b
+            } else if indi_x > point_a {
+                // indicator out range
+                indi_x
+            } else {
+                // indicator before range
+                point_a
+            };
+
+            if point_a < point_b {
+                // unplay range
                 window.paint_quad(quad(
                     Bounds {
-                        origin: point(point_a, self.origin_point.y - px(10.)),
+                        origin: point(point_a, self.origin_point.y),
                         size: Size {
                             width: point_b - point_a,
-                            height: px(30.),
+                            height: base_h,
                         },
                     },
                     Corners::default(),
-                    rgba(0xffd55f66),
-                    px(1.),
-                    rgb(0xffd55f),
+                    selected_unplay,
+                    px(0.),
+                    gpui::white(),
+                    BorderStyle::default(),
+                ));
+                // played range
+                window.paint_quad(quad(
+                    Bounds {
+                        origin: point(point_a, self.origin_point.y),
+                        size: Size {
+                            width: divide_point - point_a,
+                            height: base_h,
+                        },
+                    },
+                    Corners::default(),
+                    selected_played,
+                    px(0.),
+                    gpui::white(),
                     BorderStyle::default(),
                 ));
             }
+        }
+
+        // draw range start point
+        if let Some(start) = self.range_start {
+            let point = (bounds.size.width * start).round() - px(1.);
+            paint_dashline(window, point, self.origin_point.y - px(7.5), point_color);
+        }
+        // draw range end point
+        if let Some(end) = self.range_end {
+            let point = (bounds.size.width * end).round();
+            paint_dashline(window, point, self.origin_point.y - px(7.5), point_color);
         }
 
         // triangle size
@@ -136,24 +200,22 @@ impl Element for Timeline {
 
         let width = px(1.0 / scale);
         let height = px(20.);
-        let x = self.indicator_x(bounds);
-        let y = self.origin_point.y - px(14.);
         let color = gpui::white();
         let mut path = Path::new(self.origin_point);
 
         // paint triangle
-        path.move_to(point(x - head_size, y)); // left top
-        path.line_to(point(x + head_size, y)); // right top
-        path.line_to(point(x, y + head_size)); // bottom corner
-        path.line_to(point(x - head_size, y)); // back to start
+        path.move_to(point(indi_x - head_size, indi_y)); // left top
+        path.line_to(point(indi_x + head_size, indi_y)); // right top
+        path.line_to(point(indi_x, indi_y + head_size)); // bottom corner
+        path.line_to(point(indi_x - head_size, indi_y)); // back to start
         window.paint_path(path, color);
 
         // paint indicator line
         window.paint_quad(quad(
             Bounds {
                 origin: point(
-                    x - width / 2.0,
-                    self.origin_point.y - (height - timeline_h) / 2.,
+                    indi_x - width / 2.0,
+                    self.origin_point.y - (height - base_h) / 2.,
                 ),
                 size: Size {
                     width: width,
@@ -185,5 +247,39 @@ impl IntoElement for Timeline {
 
     fn into_element(self) -> Self::Element {
         self
+    }
+}
+
+fn paint_dashline(window: &mut Window, x: Pixels, y_start: Pixels, color: impl Into<Hsla>) {
+    let dash_height = px(4.0); // 每段虚线的高度
+    let gap_height = px(1.0); // 间隔高度
+    let width = px(1.5); // 线宽
+    let height = px(25.);
+
+    let mut current_y = y_start;
+    let end_y = y_start + height;
+
+    let color = color.into();
+
+    while current_y < end_y {
+        let current_dash_h = if current_y + dash_height > end_y {
+            end_y - current_y
+        } else {
+            dash_height
+        };
+
+        window.paint_quad(quad(
+            Bounds {
+                origin: point(x, current_y),
+                size: size(width, current_dash_h),
+            },
+            Corners::default(),
+            color,
+            px(0.),
+            gpui::white(),
+            BorderStyle::default(),
+        ));
+
+        current_y += dash_height + gap_height;
     }
 }
